5f3f0c363af78b43cc38cfb414f4faec16025ebb
Gabriel Ramos <gabriel@steeltrace.io>
2025-10-14 22:48:09 +0200

    refactor: Remove unused imports from App.tsx and MobileDrawer.tsx for cleaner code. This change enhances maintainability by eliminating unnecessary dependencies.

--- src/components/organisms/MobileDrawer/MobileDrawer.tsx
+++ src/components/organisms/MobileDrawer/MobileDrawer.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import Button from '../../atoms/Button';
 import SocialLinks from '../../molecules/SocialLinks';
 import type { CTA } from '../../../types/cta';
